f32b1b38feac0f0febaceefeb418da91b9e703e1
Kartikeya Kushwaha <96879579+kartikeyakushwaha@users.noreply.github.com>
2023-03-08 07:35:05 +0000

    added connection to database

--- app.py
+++ app.py
@@ -1,35 +1,16 @@
 from flask import Flask ,render_template
+from database import load_notes_from_db
 
 app=Flask(__name__)
 
-NOTES=[{
-  'id':1,
-  'Subject':'Mathematics',
-'Topics':'Linear_Equation_in_two_variable',
-  'link':'https://docs.google.com/presentation/d/1ReeT1l0DwpjtLfJKscTUCP2WpawG_uqCfWC-k4sidEA/edit?usp=share_link',
-  'Board':'CBSE'
-},
-       {
-  'id':2,
-  'Subject':'Mathematics',
-'Topics':'Polynomials',
-  'link':'https://drive.google.com/file/d/14DxFK-hSUnlBGea_llWyqm-MrIXMBHTC/view?usp=share_link',
-  'Board':'CBSE'
-},
-       {
-  'id':3,
-  'Subject':'Science',
-'Topics':'Electricity',
-  'link':'https://docs.google.com/presentation/d/1BoT_sbplWiqh7M0A5idB2K1pSkSjAYOCpVPPkq--Kb4/edit?usp=share_link',
-  'Board':'CBSE'
-}
-  
-]
+
+
  
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html',notes=NOTES)
+  note=load_notes_from_db()
+  return render_template('home.html',notes=note)
 
 if __name__=="__main__":
   app.run(host="0.0.0.0",debug=True)
---
+++ database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine,text
+import os
+
+db_connection_string=os.environ['DB_CONNECTION_STRING']
+
+engine=create_engine(
+  db_connection_string,
+connect_args={
+  "ssl": {
+    "ssl_ca": "/etc/ssl/cert.pem"
+  }
+})
+
+
+def load_notes_from_db():
+  with engine.connect() as conn:
+   result = conn.execute(text("select * from notes"))
+
+  note = []
+  for row in result.all():
+    note.append(row._mapping)
+  return note
+  
